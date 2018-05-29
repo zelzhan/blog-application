25f2c4d5d108002c25f70b3bf4058b24c844558e
zelzhan <elzhan.zeinulla@nu.edu.kz>
2018-05-30 01:40:13 +0600

    Login

--- app/views.py
+++ app/views.py
@@ -1,8 +1,24 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {"name":"Elzhan"}
-    return render_template("index.html", user=user)
+    user = {"username":"Elzhan"}
+    posts = [
+        {
+            'author': {'username': 'Karina'},
+            'body': 'Beautiful day in Kazakhstan!'
+        },
+        {
+            'author': {'username': 'David'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", title='home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    login = LoginForm()
+    return render_template("login.html", form = login, title="Login")
